a673de8f78d00fa084d1c49b365e18b4b67e7b22
Goelishans <goelishan22@gmail.com>
2026-07-08 17:56:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "4a76e1fa-52d7-47f6-a551-8a9a9eb020e1",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
